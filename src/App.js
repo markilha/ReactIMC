3837b2a1027101194561c88e015121b88c8548ca
Marco Antonio Monteiro de Almeida <malmeida@itesp.sp>
2022-01-31 15:40:57 -0300

    Subiu Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
 
-function App() {
+export default function App() {
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [result, setResult] = useState('')
+
+  function calcularImc(){
+    const alt = altura /100;
+    const imc = peso /(alt *alt);
+
+    if(imc < 18.6){
+      setResult(`Você está abaixo do peso! Seu IMC: ${imc.toFixed(2)}`)
+    } else   if(imc >= 18.6 && imc <24.9){
+      setResult(`Peso ideal! Seu IMC: ${imc.toFixed(2)}`)
+    }else   if(imc >= 24.9 && imc <34.9){
+      setResult(`Você esta levemente acima do peso! Seu IMC: ${imc.toFixed(2)}`)
+    } else   if(imc > 34.9){
+      setResult(`Cuidado OBESIDADE! Seu IMC: ${imc.toFixed(2)}`)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app">
+      <h1>Calculadora imc</h1>
+      <span>Vamos calcular seu imc</span>
+      <div className="area-input">
 
-export default App;
+        <input
+          type="text"
+          placeholder="Peso em (kg) Ex: 90"
+          value={peso}
+          onChange={(e)=> setPeso(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Altura em  (cm) Ex: 180"
+          value={altura}
+          onChange={(e)=> setAltura(e.target.value)}
+        />
+        <button
+        onClick={calcularImc}
+        >
+          Calcular
+        </button>
+        <dic>
+          <h2>{result}</h2>
+        </dic>
+      </div>
+
+    </div>
+
+
+  )
+}
